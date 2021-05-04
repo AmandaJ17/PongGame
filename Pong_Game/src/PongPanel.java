5a1ceb2562f448a3a4a35a0ccea94ff217e83702
AmandaJ17 <amanda.stacey17@gmail.com>
2021-05-04 13:21:41 +1200

    Creation of Sprite Class

--- Pong_Game/src/PongPanel.java
+++ Pong_Game/src/PongPanel.java
@@ -1,17 +1,42 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 import javax.swing.JPanel;
+import javax.swing.Timer; 
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener{
 
 	private final static Color BACKGROUND_COLOR = Color.black;
+	private final static int TIMER_DELAY = 5; 
+	
 	 
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOR);
+		Timer timer = new Timer(TIMER_DELAY, this);
 	}
+	
+	
+	private void paintDottedLine(Graphics g) {
+	      Graphics2D g2d = (Graphics2D) g.create();
+	         Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+	         g2d.setStroke(dashed);
+	         g2d.setPaint(Color.WHITE);
+	         g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+	         g2d.dispose();
+	 }
+	
+	@Override
+	 public void paintComponent(Graphics g) {
+	      super.paintComponent(g);
+	      paintDottedLine(g);
+	 }
+
 	@Override
 	public void keyTyped(KeyEvent event) {
 		
@@ -26,10 +51,13 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	public void keyReleased(KeyEvent event) {
 		
 	}
-
+	private void update() {
+		
+	}
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		
+		update(); 
+		repaint();
 	}
 
 	
